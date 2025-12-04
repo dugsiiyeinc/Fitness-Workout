33a19fc9a36f2c9de47c16a8a3b3ec233331d666
mohageesdiir <mgoondhe96@gmail.com>
2025-12-05 00:38:35 +0300

    add js browse form

---
+++ JAVASCRIPT/btn.js
@@ -0,0 +1,4 @@
+// section browse form
+const formBrowse = document.querySelector("#all-target");
+const muscleSelect = document.querySelector("#muscle");
+const daySelect = document.querySelector("#day");
